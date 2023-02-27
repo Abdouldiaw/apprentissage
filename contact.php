956d5f2ac8024442f10b0a472b44b4ac828400ae
7749710d@gmail.com <7749710d@gmail.com>
2023-02-27 16:45:31 +0100

    je suis intelligent

---
+++ contact.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Formulaire de contact</title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap/css/bootstrap.min.css">
+    </head>
+
+    <body>
+      <div class="container" id="container">
+        <div class="form-container sing-up-container">
+          <form action="#">
+            <h1>Formulaire de contact</h1>
+          </form>
+          <input type="text" placeholder="enter your name">
+          <input type="text" placeholder="enter a valide email adress ">
+          <input type="text" placeholder="enter your messages">
+          <button>Nous faire parvenir</button>
+
+        </div>
+
+      </div>
+    </body>
